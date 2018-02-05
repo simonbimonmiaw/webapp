a1d5d7c86ea218ae5d67833f59514c942f2e026e
Andreas <asf1994@gmail.com>
2018-02-05 13:51:50 +0100

    v1
    Lektien mandag d. 5

---
+++ page1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html la="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Chang'a'later</title>
+    <link href="css/styles.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <!-- Heading + seach form -->
+    <main>
+      <h1>Chang'a'later</h1>
+      <h5>- The internet's converter for useless currencies</h5>
+
+      <!-- Search form -->
+      <form action="" method="get">
+        <select name="currency">
+          <option value="USD">USD</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+        <input type="text" name="search-input" placeholder="Amount">
+        <input type="submit">
+      </form>
+      <!-- Search form end -->
+      
+    </main>
+    <!-- heading + search form end -->
+
+    <!-- Browse currency start  -->
+    <section>
+
+      <!-- For each in category -->
+      <h2>... Or browse currency: </h2>
+      <a href="" class="category-link">
+      <!-- end foreach -->
+
+    </section>
+    <!-- Browse currency end  -->
+  </body>
+</html>
---
+++ page2.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html la="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Chang'a'later</title>
+    <link href="css/styles.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <!-- Heading + seach form -->
+    <main>
+      <h1>Chang'a'later</h1>
+      <h5>- The internet's converter for useless currencies</h5>
+
+      <!-- Search form -->
+      <form action="" method="get">
+        <select name="currency">
+          <option value="USD">USD</option>
+          <option value="saab">DKK</option>
+          <option value="opel">PESOS</option>
+          <option value="audi">NKK</option>
+        </select>
+        <input type="text" name="search-input" placeholder="Amount">
+        <input type="submit">
+      </form>
+      <!-- End search form -->
+
+    </main>
+    <!-- End heading + search form -->
+
+    <!-- Start browse currency -->
+    <section>
+      <h2>... Or browse currency: </h2>
+      <article>
+        <h3>Category: .... </h3>
+        <button>X</button>
+        <div>
+        <img src="" alt="Billede af fugl">
+        <h5>Some parrot</h5>
+
+        <!-- ... For each  -->
+        <form class="" action="" method="post">
+          <select name="currency">
+            <option value="USD">USD</option>
+            <option value="saab">DKK</option>
+            <option value="opel">NKK</option>
+            <option value="audi">SKK</option>
+          </select>
+          <input type="hidden" value="id" name="id">
+          <input type="text" name="amount" placeholder="Amount">
+          <input type="submit">
+        </form>
+        <!-- Endforeach -->
+
+      </article>
+    </section>
+    <!-- End browse currency -->
+
+  </body>
+</html>
