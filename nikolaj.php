efb6fc126faa0033d1d7888b95d336d0b903c2dd
Kiil <Nikolajkiil@live.dk>
2018-02-07 08:38:29 +0100

    Search
    Created a searchbar and added scss

---
+++ nikolaj.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Convertinator</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/nikolaj.css">
+  </head>
+  <body>
+
+    <!-- Heading -->
+    <header class="header">
+      <h1 class="header__heading">Convertinator</h1>
+      <h5 class="header__subheading">- The internet's converter for useless currencies</h5>
+    </header>
+
+      <!-- Search form -->
+    <main class="main__form">
+      <form class="form__design" action="" method="get">
+        <select class="select--currency" name="currency">
+          <option class="select_option" value="DKK">DKK</option>
+          <option class="select_option" value="USD">USD</option>
+          <option class="select_option" value="SKK">SKK</option>
+          <option class="select_option" value="NKK">NKK</option>
+        </select>
+        <input class="form__text" type="text" name="search-input" placeholder="Amount">
+        <input class="form__submit" type="submit" value="Random">
+      </form>
+      <!-- Search form end -->
+
+    </main>
+    <!-- heading + search form end -->
+
+    <!-- Browse currency start  -->
+    <section>
+
+      <!-- Categories -->
+      <h2>... Or browse currency: </h2>
+
+
+      <ul>
+        <li class="">Foods</li>
+        <li class="">Minerals</li>
+        <li class="">Ships</li>
+        <li class="">Cars</li>
+        <li class="">Animals</li>
+        <li class="">Guns</li>
+        <li class="">Houses</li>
+        <li class="">Drinks</li>
+        <li class="">Electronic</li>
+      <ul>
+      <!-- Categories end -->
+
+    </section>
+    <!-- Browse currency end  -->
+  </body>
+</html>
