10e2123f178574c6d03d27d5fd8e7fca72e8af1d
F1rr1n <sebmorgen@hotmail.com>
2018-02-07 10:00:23 +0100

    Katagorigrid

---
+++ Seb.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html la="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Chang'a'later</title>
+
+    <link rel="stylesheet" href="/css/styles.css">
+    <link href="css/seb.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/nikolaj.css">
+  </head>
+  <body>
+
+    <!-- Heading + seach form -->
+    <main>
+      <h1>Chang'a'later</h1>
+      <h5>- The internet's converter for useless currencies</h5>
+
+      <!-- Search form -->
+      <form action="" method="get">
+        <select name="currency">
+          <option value="USD">USD</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+        <input type="text" name="search-input" placeholder="Amount">
+        <input type="submit">
+      </form>
+      <!-- Search form end -->
+
+    </main>
+    <!-- heading + search form end -->
+
+    <!-- Browse currency start  -->
+    <section>
+
+      <!-- Categories -->
+      <h2>... Or browse currency: </h2>
+      <ul class="grid-container">
+        <li class="grid-item"><a href="#">Food</a></li>
+        <li class="grid-item"><a href="#">Minerals</a></li>
+        <li class="grid-item"><a href="#">Ships</a></li>
+        <li class="grid-item"><a href="#">Cars</a></li>
+        <li class="grid-item"><a href="#">Animals</a></li>
+        <li class="grid-item"><a href="#">Guns</a></li>
+        <li class="grid-item"><a href="#">Houses</a></li>
+        <li class="grid-item"><a href="#">Drinks</a></li>
+        <li class="grid-item"><a href="#">Electronics</a></li>
+      </ul>
+      <!-- Categories end -->
+
+    </section>
+    <!-- Browse currency end  -->
+  </body>
+</html>
