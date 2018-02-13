a579d96386f45eb9892d8a7850bf311252affc65
Andreas <asf1994@gmail.com>
2018-02-13 12:26:33 +0100

    V 2.2
    Added lots of items to vue data

--- javascript/vue-script.js
+++ javascript/vue-script.js
@@ -1,35 +1,93 @@
 var app7 = new Vue({
   el: '#test',
   data: {
-    animals: [
+    items: [
       {
-        id: 0, name: 'Seahorse', image: 'images/seahorse.jpg', value: 900
+        name: 'Seahorse', image: 'images/seahorse.jpg', value: 900, category: 'animals'
       },
       {
-        id: 1, name: 'Chameleon', image: 'images/chameleon.jpg', value: 250
+        name: 'Chameleon', image: 'images/chameleon.jpg', value: 250, category: 'animals'
       },
       {
-        id: 2, name: 'Tibetan Mastiff', image: 'images/tibetan-mastiff.jpg', value: 42500
+        name: 'Tibetan Mastiff', image: 'images/tibetan-mastiff.jpg', value: 42500, category: 'animals'
       },
       {
-        id: 3, name: 'Marmoset Monkey', image: 'images/marmoset-monkey.jpg', value: 23700
+        name: 'Marmoset Monkey', image: 'images/marmoset-monkey.jpg', value: 23700
       },
-      { id: 4, name: 'Icelandic Hourse', image: 'images/icelandic-horse.jpg', value: 15000
-      },
-      {
-        id: 5, name: 'Chihuahua', image: 'images/chihuahua.jpg', value: 3500
+      { name: 'Icelandic Hourse', image: 'images/icelandic-horse.jpg', value: 15000, category: 'animals'
       },
       {
-        id: 6, name: 'Hedgehog', image: 'images/hedgehog.jpg', value: 1200
+        name: 'Chihuahua', image: 'images/chihuahua.jpg', value: 3500, category: 'animals'
       },
       {
-        id: 7, name: 'Cuvier\'s Dwarf Caiman', image: 'images/dwarf-caiman.jpg', value: 1800
+        name: 'Hedgehog', image: 'images/hedgehog.jpg', value: 1200, category: 'animals'
       },
       {
-        id: 8, name: 'Mini Donkey', image: 'images/mini-donkey.jpg', value: 8000
+        name: 'Cuvier\'s Dwarf Caiman', image: 'images/dwarf-caiman.jpg', value: 1800, category: 'animals'
       },
       {
-        id: 9, name: 'Llama', image: 'images/llama.jpg', value: 2500
+        name: 'Mini Donkey', image: 'images/mini-donkey.jpg', value: 8000, category: 'animals'
+      },
+      {
+        name: 'Llama', image: 'images/llama.jpg', value: 2500, category: 'animals'
+      },
+      {
+        id: 0, name: 'Seat Mii', image: 'images/seat-mii.jpg', value: 80000
+      },
+      {
+        id: 1, name: 'Audi a4', image: 'images/audi-a4.jpg', value: 457000
+      },
+      {
+        id: 2, name: 'Lamborghini Aventador Roadster', image: 'images/lamborghini', value: 8500000
+      },
+      {
+        id: 3, name: 'Ferrari 335 Sport', image: 'images/ferrari-335.jpg', value: 250000000
+      },
+      { id: 4, name: 'Honda Civic', image: 'images/honda-civic.jpg', value: 249000
+      },
+      {
+        id: 5, name: 'Mercedes S350', image: 'images/mercedes-s350.jpg', value: 1700000
+      },
+      {
+        id: 6, name: 'Koenigsegg One', image: 'images/koenigsegg-one.jpg', value: 10400000
+      },
+      {
+        id: 7, name: 'Bugatti Veyron Grand Sport Ettorre', image: 'images/bugatti-veyron.jpg', value: 17300000
+      },
+      {
+        id: 8, name: 'Pagani Huayra', image: 'images/pagani-huayra.jpg', value: 9700000
+      },
+      {
+        id: 9, name: 'Hyundai i10', image: 'images/hyundai-i10.jpg', value: 84888
+      },
+      {
+        id: 0, name: 'Bel Air Spec Manor, Florida', image: 'images/bel-air-spec-manor-florida.jpg', value: 1519202720
+      },
+      {
+        id: 1, name: 'Briar Patch, The Hamptons', image: 'images/briar-patch.jpg', value: 850000000
+      },
+      {
+        id: 2, name: 'Il Palmetto, Palm Beach', image: 'images/il-palmetto.jpg', value: 832000000
+      },
+      {
+        id: 3, name: 'Fifth Avenue Duplex, New York City', image: 'images/fifth-avenue-duplex.jpg', value: 729483283
+      },
+      { id: 4, name: 'Brookline Estate, Boston', image: 'images/bookline-estate.jpg', value: 547112462
+      },
+      {
+        id: 5, name: 'Holmby Hills Villa, Los Angeles', image: 'images/holby-hills-villa.jpg', value: 534954407
+      },
+      {
+        id: 6, name: 'Beverly Hills Mansion, Los Angeles', image: 'images/beverly-hills-mansion.jpg', value: 516717325
+      },
+      {
+        id: 7, name: 'Edie Goetz Mansion, Los Angeles', image: 'images/eddie-goetz-mansion.jpg', value: 480243161
+      },
+      {
+        id: 8, name: 'Shitty old house in Denmark', image: 'images/shitty-old-house.jpg', value: 75000
+      },
+      {
+        id: 9, name: 'House in kværndrup', image: 'images/house-in-kværndrup.jpg', value: 95000
       }
     ],
     randomDisplay: {
@@ -84,13 +142,13 @@ var app7 = new Vue({
         var money = this.convertCurrency(currency, userInput);
 
         // Receive array length and generate random number between 0 and array length
-        var arrayCount = this.animals.length;
+        var arrayCount = this.items.length;
         var randomNum = Math.floor((Math.random() * arrayCount));
 
         // Display random array item and amount
-        this.randomDisplay.name = this.animals[randomNum].name;
-        this.randomDisplay.image = this.animals[randomNum].image;
-        this.randomDisplay.amount = Math.floor(money / this.animals[randomNum].value);
+        this.randomDisplay.name = this.items[randomNum].name;
+        this.randomDisplay.image = this.items[randomNum].image;
+        this.randomDisplay.amount = Math.floor(money / this.items[randomNum].value);
         this.randomDisplayIsActive = true;
       }
     }
