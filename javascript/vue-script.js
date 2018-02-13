5e779ad91460645b3b9bb0648d613fb8c795194c
F1rr1n <sebmorgen@hotmail.com>
2018-02-13 12:54:08 +0100

    MORE More data in items

--- javascript/vue-script.js
+++ javascript/vue-script.js
@@ -104,9 +104,23 @@ var app7 = new Vue({
 	    {
 		    name: 'Dry martini', image: 'images/dry-martini.jpg', value: 75
 	    },
+
       {name: 'Mauser 98K', image: 'images/mauser.jpg', value: 12000, category: 'guns'
       },
+      {id:0, name: 'Tiger', image: 'images/tigercub.jpg', value: 45000, category: 'animals'
+      },
+      {
+      id:1, name: 'Horse', image: 'images/horse.jpg', value: 30000, category: 'animals'
+      },
+      {id:2, name: 'Labrador Retriever', image: 'images/dog.jpg', value: 8000, category: 'animals'},
 
+      {id:3, name: 'Turtle', image: 'images/turtle.jpg', value: 60, category: 'animals'},
+
+      { id:4, name: 'Camel', image: 'images/camel.jpg', value: 4000, category: 'animals'},
+
+      { id:5, name: 'Sibirisk dværghamster', image: 'images/hamster.jpg', value: 150, category: 'animals'},
+
+      { id:6, name: 'Cheeseburger', image: 'images/Cheeseburger.jpg', value: 10, category: 'foods'},
       {
         name: 'Desert Eagle', image: 'images/deserteagle.jpg', value: 9000, category: 'guns'
       },
